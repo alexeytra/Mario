6fd501967e015577392323d7855a4e42afed5b3b
alex <alekceytr@mail.ru>
2018-11-21 01:59:57 +0800

    add table

--- core/src/com/alex/mariobros/MarioBros.java
+++ core/src/com/alex/mariobros/MarioBros.java
@@ -10,6 +10,8 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MarioBros extends Game {
 	public SpriteBatch batch;
+	public static final int V_WIDTH = 400;
+	public static final int V_HEIGHT = 208;
 
 	
 	@Override
--- core/src/com/alex/mariobros/Screens/PlayScreen.java
+++ core/src/com/alex/mariobros/Screens/PlayScreen.java
@@ -1,18 +1,28 @@
 package com.alex.mariobros.Screens;
 
 import com.alex.mariobros.MarioBros;
+import com.alex.mariobros.Scenes.Hud;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class PlayScreen implements Screen {
     private MarioBros game;
-    Texture texture;
+    private OrthographicCamera orthographicCamera;
+    private Viewport gamePort;
+    private Hud hud;
 
     public PlayScreen(MarioBros game){
         this.game = game;
-        texture = new Texture("badlogic.jpg");
+        orthographicCamera = new OrthographicCamera();
+        gamePort = new FitViewport(MarioBros.V_WIDTH, MarioBros.V_HEIGHT, orthographicCamera);
+        hud = new Hud(game.batch);
     }
 
     @Override
@@ -22,17 +32,17 @@ public class PlayScreen implements Screen {
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        game.batch.begin();
-        game.batch.draw(texture, 0, 0);
-        game.batch.end();
+
+        game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
+        hud.stage.draw();
 
     }
 
     @Override
     public void resize(int width, int height) {
-
+        gamePort.update(width, height);
     }
 
     @Override
